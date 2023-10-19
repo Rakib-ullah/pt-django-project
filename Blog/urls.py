42532b7665a5d7fb07f5b5c5f6bd0c195ddea626
Rakib-ullah <rakinullah19730@gmail.com>
2023-10-19 21:35:57 +0600

    dynamic html done

--- Blog/urls.py
+++ Blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage),
+    path('about/', views.about),
 ]
 
--- Blog/views.py
+++ Blog/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 
+blogdata = [
+    {
+        "title": "ফ্রাঙ্কফুর্ট বইমেলা শুরু, থাকছে বাংলাদেশও",
+        "content": "জার্মানির ফ্রাঙ্কফুর্টে বইমেলার ৭৫তম আসর বসছে। জার্মানি এবং সেই সঙ্গে বিশ্বের সাহিত্য ও সংস্কৃতিকে এগিয়ে"
+                   " নিতে এই মেলার রয়েছে অপরিসীম অবদান",
+        "author": "সরাফ আহমেদ",
+        "post_datetime": " ১৯ অক্টোবর ২০২৩, ১৯: ০৩",
 
-# Create your views here.
+    }
+
+]
+
+
 def homepage(request):
-    return render(request, "home.html")
+    return render(request, "home.html", {"blogdata": blogdata})
+
+
+def about(request):
+    return render(request, "about.html", {"title": "about page"})
