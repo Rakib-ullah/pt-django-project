71178e805f2d205d8b8583219a26e8b9215ba622
Rakib-ullah <rakinullah19730@gmail.com>
2023-11-07 21:36:19 +0600

    new post update delete

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
-from .views import PostListview, PostDetailsView, PostCreateView
+from .views import PostListview, PostDetailsView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path('', PostListview.as_view(), name="blog home"),
     path('post/<int:pk>', PostDetailsView.as_view(), name="post-details"),
     path('post/new/', PostCreateView.as_view(), name="post-create"),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name="post-update"),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name="post-delete"),
     path('about/', views.about, name="blog about"),
 
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 blogdata = [
     {
@@ -61,3 +62,31 @@ class PostCreateView(CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class PostUpdateView(UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'post_forms.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(UserPassesTestMixin,DeleteView):
+    model = Post
+    success_url = '/'
+    template_name = 'post_delete.html'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
